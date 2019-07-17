ecb08b11508cefffe9283a3bf0f656319f54c2a4
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-17 15:20:59 +0200

    Completamento esercizio.

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Creare una griglia formata da 8x8 quadratini tutti bianchi.
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
+
+$( document ).ready(function() {
+
+  // definizione per click su quadrati con sfondo verde
+  $("div .green").click(function() {
+    $(this).css("background", "green");
+  });
+
+  // definizione per click quadrati sfondo rosso
+  $("div .red").click(function() {
+    $(this).css("background", "red");
+  });
+
+
+});
